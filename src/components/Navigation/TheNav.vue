fadfc4fd230a416408b401a412a839e224926e88
Anton Borisov <thesameanton@Anton-MBP.telekom.ip>
2024-02-08 20:38:29 +0100

    feat: navBar

--- src/components/Navigation/TheNav.vue
+++ src/components/Navigation/TheNav.vue
@@ -1,7 +1,13 @@
 <template>
   <nav class="sticky bottom-0 z-20 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="(icons, page) in navItems" :key="page" :href="`#${page}`">
+      <NavItem
+        v-for="(icons, page) in navItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'bg-gray-200 duration-150 pointer-events-none': page === currentPage }"
+        @click="currentPage = page"
+      >
         <component :is="icons" class="h-6 w-6" />
         {{ page }}
       </NavItem>
@@ -10,15 +16,31 @@
 </template>
 
 <script setup>
-//Icons
-import { ChartBarIcon, ClockIcon, ListBulletIcon } from '@heroicons/vue/24/outline/index.js'
-
 //Components
+import { ChartBarIcon, ClockIcon, ListBulletIcon } from '@heroicons/vue/24/outline/index.js'
 import NavItem from '@/components/Navigation/NavItem.vue'
 
+import { PAGE } from '@/constants.js'
+import { ref } from 'vue'
+
 const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon
+  [PAGE.TIMELINE]: ClockIcon,
+  [PAGE.ACTIVITIES]: ListBulletIcon,
+  [PAGE.PROGRESS]: ChartBarIcon
 }
+
+const normalizeGetPath = () => {
+  const hash = window.location.hash.slice(1)
+
+  if (Object.keys(navItems).includes(hash)) {
+    return hash
+  }
+
+  window.location.hash = PAGE.TIMELINE
+
+  return PAGE.TIMELINE
+}
+
+const currentPage = ref(normalizeGetPath())
+
 </script>
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const PAGE = Object.freeze({
+  TIMELINE: 'timeline',
+  ACTIVITIES: 'activities',
+  PROGRESS: 'progress',
+})
