1b2ef9b657081254ab0c6403d80177362dc21eaf
Anton Borisov <ant.b.a.2002.17@gmail.com>
2024-07-07 15:43:30 +0200

    fix: activity empty ing

--- src/components/TheActivityEmpty.vue
+++ src/components/TheActivityEmpty.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="grow flex flex-col gap-4 p-8 text-center items-center justify-center text-2xl">
+  <div class="flex flex-col items-center justify-center grow text-center text-2xl gap-4">
     <img
       src="@/assets/img/no_activities.svg"
       alt="No activities"
+      class="max-w-full max-h-[50vh] mb-4 object-contain"
     />
     <span class="text-gray-400">You don't have any activity.</span>
   </div>
